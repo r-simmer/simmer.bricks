54b231fb007aade9399e3767133bb47cec3c933d
Iñaki Úcar <i.ucar86@gmail.com>
2018-01-14 03:08:48 +0100

    close #3: implement do_parallel

---
+++ R/do_parallel.R
@@ -0,0 +1,102 @@
+#' Perform Parallel Tasks
+#'
+#' This brick encapsulates the activity of \code{n} workers running parallel
+#' sub-trajectories.
+#'
+#' @inheritParams simmer::clone
+#' @inheritParams simmer::get_capacity
+#' @param ... parallel sub-trajectories.
+#' @param wait if \code{TRUE}, the arrival waits until all parallel sub-trajectories
+#' are finished; if \code{FALSE}, the arrival continues as soon as the first
+#' parallel task ends.
+#'
+#' @return Returns the following chain of activities: \code{\link[simmer]{clone}}
+#' > \code{\link[simmer:clone]{synchronize}} (> \code{\link[simmer:send]{wait}}
+#' > \code{\link[simmer:send]{untrap}} if \code{wait=FALSE}) (see examples below).
+#' @export
+#'
+#' @examples
+#' env <- simmer()
+#' signal <- function() get_name(env)
+#'
+#' task.1 <- trajectory("task 1") %>%
+#'   timeout(function() rexp(1))
+#' task.2 <- trajectory("task 2") %>%
+#'   timeout(function() rexp(1))
+#'
+#' ## These are equivalent:
+#' trajectory() %>%
+#'   do_parallel(
+#'     env, wait = TRUE,
+#'     task.1,
+#'     task.2
+#'   )
+#'
+#' trajectory() %>%
+#'   clone(
+#'     n = 3,
+#'     trajectory("original") %>%
+#'       trap(signal) %>%
+#'       wait() %>%
+#'       wait() %>%
+#'       untrap(signal),
+#'     task.1[] %>%
+#'       send(signal),
+#'     task.2[] %>%
+#'       send(signal)) %>%
+#'   synchronize(wait = TRUE)
+#'
+#' ## These are equivalent:
+#' trajectory() %>%
+#'   do_parallel(
+#'     env, wait = FALSE,
+#'     task.1,
+#'     task.2
+#'   )
+#'
+#' trajectory() %>%
+#'   clone(
+#'     n = 3,
+#'     trajectory("original") %>%
+#'       trap(signal),
+#'     task.1[] %>%
+#'       send(signal),
+#'     task.2[] %>%
+#'       send(signal)) %>%
+#'   synchronize(wait = FALSE) %>%
+#'   wait() %>%
+#'   untrap(signal)
+#'
+do_parallel <- function(.trj, .env, ..., wait=TRUE, mon_all = FALSE) {
+  if (!inherits(.env, "simmer"))
+    stop("Argument '", deparse(substitute(.env)), "' is not a 'simmer' environment")
+
+  tasks <- list(...)
+  types <- sapply(tasks, inherits, what="trajectory")
+
+  if (!all(types)) {
+    dots <- substitute(list(...))[-1]
+    args <- paste(sapply(dots, deparse)[[types]], collapse="', '")
+    stop("Arguments '", args, "' are not 'simmer' trajectories")
+  }
+
+  signal <- function() get_name(.env)
+  tasks <- lapply(tasks, `[`)
+  tasks <- lapply(tasks, send, signals=signal)
+
+  if (wait) {
+    original <- trajectory("original") %>%
+      wait_until(signal, length(tasks))
+    append <- trajectory()
+  } else {
+    original <- trajectory("original") %>%
+      trap(signal)
+    append <- trajectory() %>%
+      wait() %>%
+      untrap(signal)
+  }
+
+  do.call(clone, c(.trj, length(tasks)+1, original, tasks)) %>%
+    synchronize(wait=wait) %>%
+    join(append)
+}
---
+++ tests/testthat/test-do_parallel.R
@@ -0,0 +1,34 @@
+context("do_parallel")
+
+test_that("a missing environment/trajectory fails", {
+  expect_error(do_parallel(trajectory(), NULL, trajectory()))
+  expect_error(do_parallel(trajectory(), simmer(), NULL))
+})
+
+test_that("do_parallel generates the correct sequence of activities", {
+  env <- simmer()
+
+  t <- trajectory() %>%
+    do_parallel(env, trajectory(), trajectory(), wait=TRUE)
+
+  expect_equal(length(t), 2)
+  expect_equal(get_n_activities(t), 8)
+  expect_output(print(t[[1]]), paste0(
+    "Clone.*3.*",
+    "Fork 1.*Trap.*Wait.*Wait.*UnTrap.*",
+    "Fork 2.*Send.*Fork 3.*Send"))
+  expect_output(print(t[[2]]), "Synchronize.*1")
+
+  t <- trajectory() %>%
+    do_parallel(env, trajectory(), trajectory(), wait=FALSE)
+
+  expect_equal(length(t), 4)
+  expect_equal(get_n_activities(t), 7)
+  expect_output(print(t[[1]]), paste0(
+    "Clone.*3.*",
+    "Fork 1.*Trap.*",
+    "Fork 2.*Send.*Fork 3.*Send"))
+  expect_output(print(t[[2]]), "Synchronize.*0")
+  expect_output(print(t[[3]]), "Wait")
+  expect_output(print(t[[4]]), "UnTrap")
+})
