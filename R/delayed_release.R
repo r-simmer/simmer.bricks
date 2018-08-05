b72f19b1fe238e8cb31b2ab927f90ff5b637dbc0
Iñaki Ucar <i.ucar86@gmail.com>
2018-08-05 19:51:08 +0200

    close #4: implement delayed_release_selected (#9)
    * simplify delayed_release and bump required simmer version
    
    * close #4: implement delayed_release_selected
    
    * update NEWS

--- R/delayed_release.R
+++ R/delayed_release.R
@@ -15,29 +15,25 @@
 #' @export
 #'
 #' @examples
-#' env <- simmer()
-#'
-#' ## These are equivalent if the resource is non-preemptive:
+#' ## These are equivalent for a non-preemptive resource:
 #' trajectory() %>%
-#'   delayed_release(env, "res1", 5, 1)
+#'   delayed_release("res1", 5, 1)
 #'
 #' trajectory() %>%
 #'   clone(
 #'     2,
 #'     trajectory() %>%
-#'       set_capacity("res1", function()
-#'         get_capacity(env, "res1") - 1) %>%
+#'       set_capacity("res1", -1, mod="+") %>%
 #'       release("res1", 1),
 #'     trajectory() %>%
 #'       timeout(5) %>%
-#'       set_capacity("res1", function()
-#'         get_capacity(env, "res1") + 1)
+#'       set_capacity("res1", 1, mod="+")
 #'   ) %>%
 #'   synchronize(wait=FALSE)
 #'
-#' ## These are equivalent if the resource is preemptive:
+#' ## These are equivalent for a preemptive resource:
 #' trajectory() %>%
-#'   delayed_release(env, "res2", 5, 1, preemptive=TRUE)
+#'   delayed_release("res2", 5, 1, preemptive=TRUE)
 #'
 #' trajectory() %>%
 #'   clone(
@@ -45,26 +41,23 @@
 #'     trajectory() %>%
 #'       release("res2", 1),
 #'     trajectory() %>%
-#'       set_prioritization(function()
-#'         get_prioritization(env) + c(rep(.Machine$integer.max, 2), 0)) %>%
+#'       set_prioritization(c(rep(.Machine$integer.max, 2), 0)) %>%
 #'       seize("res2", 1) %>%
 #'       timeout(5) %>%
 #'       release("res2", 1)
 #'   ) %>%
 #'   synchronize(wait=FALSE)
 #'
-delayed_release <- function(.trj, .env, resource, task, amount=1, preemptive=FALSE, mon_all=FALSE) {
+delayed_release <- function(.trj, resource, task, amount=1, preemptive=FALSE, mon_all=FALSE) {
   if (!preemptive) {
     .clone <- clone(
       .trj, 2,
       trajectory() %>%
-        set_capacity(resource, function()
-          get_capacity(.env, resource) - amount) %>%
+        set_capacity(resource, -amount, mod="+") %>%
         release(resource, amount),
       trajectory() %>%
         timeout(task) %>%
-        set_capacity(resource, function()
-          get_capacity(.env, resource) + amount)
+        set_capacity(resource, amount, mod="+")
     )
   } else {
     .clone <- clone(
@@ -76,6 +69,35 @@ delayed_release <- function(.trj, .env, resource, task, amount=1, preemptive=FAL
         seize(resource, amount) %>%
         timeout(task) %>%
         release(resource, amount)
+    )
+  }
+
+  .clone %>% synchronize(wait=FALSE, mon_all=mon_all)
+}
+
+#' @rdname delayed_release
+#' @export
+delayed_release_selected <- function(.trj, task, amount=1, preemptive=FALSE, mon_all=FALSE) {
+  if (!preemptive) {
+    .clone <- clone(
+      .trj, 2,
+      trajectory() %>%
+        set_capacity_selected(-amount, mod="+") %>%
+        release_selected(amount),
+      trajectory() %>%
+        timeout(task) %>%
+        set_capacity_selected(amount, mod="+")
+    )
+  } else {
+    .clone <- clone(
+      .trj, 2,
+      trajectory() %>%
+        release_selected(amount),
+      trajectory() %>%
+        set_prioritization(c(rep(.Machine$integer.max, 2), 0)) %>%
+        seize_selected(amount) %>%
+        timeout(task) %>%
+        release_selected(amount)
     )
   }
 
--- tests/testthat/test-delayed_release.R
+++ tests/testthat/test-delayed_release.R
@@ -2,7 +2,7 @@ context("delayed_release")
 
 test_that("delayed_release generates the correct sequence of activities", {
   t <- trajectory() %>%
-    delayed_release(env, "dummy", 5, 3)
+    delayed_release("dummy", 5, 3)
 
   expect_equal(length(t), 2)
   expect_equal(get_n_activities(t), 6)
@@ -13,7 +13,18 @@ test_that("delayed_release generates the correct sequence of activities", {
   expect_output(print(t[[2]]), "Synchronize.*0")
 
   t <- trajectory() %>%
-    delayed_release(env, "dummy_preemptive", 5, 3, preemptive=TRUE)
+    delayed_release_selected(5, 3)
+
+  expect_equal(length(t), 2)
+  expect_equal(get_n_activities(t), 6)
+  expect_output(print(t[[1]]), paste0(
+    "Clone.*2.*",
+    "Fork 1.*SetCapacity.*Release.*3.*",
+    "Fork 2.*Timeout.*5.*SetCapacity"))
+  expect_output(print(t[[2]]), "Synchronize.*0")
+
+  t <- trajectory() %>%
+    delayed_release("dummy_preemptive", 5, 3, preemptive=TRUE)
 
   expect_equal(length(t), 2)
   expect_equal(get_n_activities(t), 7)
@@ -21,5 +32,16 @@ test_that("delayed_release generates the correct sequence of activities", {
     "Clone.*2.*",
     "Fork 1.*Release.*dummy_preemptive.*3.*",
     "Fork 2.*SetPrior.*Seize.*dummy_preemptive.*3.*Timeout.*5.*Release.*dummy_preemptive.*3"))
+  expect_output(print(t[[2]]), "Synchronize.*0")
+
+  t <- trajectory() %>%
+    delayed_release_selected(5, 3, preemptive=TRUE)
+
+  expect_equal(length(t), 2)
+  expect_equal(get_n_activities(t), 7)
+  expect_output(print(t[[1]]), paste0(
+    "Clone.*2.*",
+    "Fork 1.*Release.*3.*",
+    "Fork 2.*SetPrior.*Seize.*3.*Timeout.*5.*Release.*3"))
   expect_output(print(t[[2]]), "Synchronize.*0")
 })
